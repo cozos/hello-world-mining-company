75c07d1ff6570cb85b32849cce1f4734f230c230
anava <anton.diaz.nava@gmail.com>
2015-05-13 19:51:45 -0400

    Move some logic to Unit getActions(...)

--- ExampleBot/src/com/scbot/game/agent/ResourceManagerAgent.java
+++ ExampleBot/src/com/scbot/game/agent/ResourceManagerAgent.java
@@ -1,16 +1,30 @@
 package com.scbot.game.agent;
 
-import com.scbot.game.actions.Action;
+import com.scbot.game.actions.IAction;
 import com.scbot.game.state.GameState;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
  * Created by User1 on 5/10/2015.
  */
 public class ResourceManagerAgent implements IAgent{
+
     @Override
-    public Collection<Action> getActions(GameState state) {
-        return null;
+    public Collection<IAction> getActions(GameState state) {
+
+        Collection<IUnit> idleWorkers = state.getIdleWorkers();
+        Collection<IAction> actions = new ArrayList<>();
+
+        for(IUnit unit : idleWorkers){
+            Collection<IAction> unitActions = unit.getActions(state);
+            for (IAction action : unitActions){
+                state.occupyMineral(action.getTargetID());
+            }
+            actions.addAll(unitActions);
+        }
+
+        return actions;
     }
 }
